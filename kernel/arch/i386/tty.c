1336cb37af3cc7c090a5fc021062b9c34a3889d6
reina314 <ryugureina314@gmail.com>
2025-02-17 15:19:44 +0900

    update: support for '\n' and other minor changes

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -11,14 +11,14 @@ static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
 
-static size_t terminal_row;
-static size_t terminal_column;
+static size_t terminal_x;
+static size_t terminal_y;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
 
 void terminal_initialize(void) {
-	terminal_row = 0;
-	terminal_column = 0;
+	terminal_x = 0;
+	terminal_y = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
@@ -40,11 +40,27 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	
+	switch (uc) {
+	case '\n': 
+		terminal_x = 0;
+		terminal_y++;
+		break;
+
+	default:
+		terminal_putentryat(uc, terminal_color, terminal_x, terminal_y);	
+		terminal_x++;
+		break;
+	}
+
+	if (terminal_x == VGA_WIDTH) {
+		terminal_x = 0;
+		terminal_y++;
+	}
+		
+	if (terminal_y == VGA_HEIGHT) {
+		terminal_x = 0;
+		terminal_y = 0;
 	}
 }
 
