024846595083050c4f291ac034c071a8d853e905
reina314 <ryugureina314@gmail.com>
2025-02-18 20:18:00 +0900

    update: add idt_install

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 #include <kernel/gdt.h>
+#include <kernel/idt.h>
 #include <kernel/tty.h>
 
 void kernel_main(void)
 {
 	gdt_install();
+	idt_install();
 	terminal_initialize();
 }
