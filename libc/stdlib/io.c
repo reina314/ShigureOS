1675b815810023cbb4e88cb18959de74f577d8ed
reina314 <ryugureina314@gmail.com>
2025-02-20 00:42:30 +0900

    update: implement inb()

--- libc/stdlib/io.c
+++ libc/stdlib/io.c
@@ -8,3 +8,13 @@ void outb(uint16_t port, uint8_t value)
 {
     __asm__ volatile("outb %1, %0" ::"dN"(port), "a"(value));
 }
+
+/// @brief Read a byte from the specified port
+/// @param port I/O port number (16bit)
+/// @return A byte read for the port (8bit)
+uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ volatile("inb %1, %0" : "=a"(ret) : "dN"(port));
+    return ret;
+}
