c72713d2b75c7232ad46b0fef086dfb3ddba82f8
reina314 <ryugureina314@gmail.com>
2025-02-20 00:48:42 +0900

    update: initialize keyboard & serial driver & debug

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,13 +4,24 @@
 #include <kernel/idt.h>
 #include <kernel/isr.h>
 #include <kernel/irq.h>
+#include <kernel/serial.h>
 #include <kernel/tty.h>
+#include <kernel/kb.h>
 
 void kernel_main(void)
 {
+	terminal_initialize();
 	gdt_install();
 	idt_install();
 	isr_install();
 	irq_install();
-	terminal_initialize();
+	serial_initialize(SERIAL_COM1_BASE);
+	keyboard_install();
+	printf("Everything setup correctly\n");
+
+	asm volatile("sti");
+	__asm__ volatile("sti"); // Enable interrupts
+
+	while (1)
+		;
 }
