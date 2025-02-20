c86c3dd10a9774f83353886f2973b99d43e5d9cc
reina314 <ryugureina314@gmail.com>
2025-02-20 18:52:16 +0900

    feat: timer

---
+++ kernel/arch/i386/timer.c
@@ -0,0 +1,38 @@
+#include <kernel/timer.h>
+#include <kernel/irq.h> // for irq register
+
+/// @brief How many times timer_handler is called since booted
+unsigned int timer_ticks = 0;
+
+/// @brief Time passed since booted in seconds
+int seconds_passed = 0;
+
+/// @brief The timer fires 18.222 times per second
+/// @param r Not used
+void timer_handler(struct regs *r)
+{
+    r = r; // avoid unused parameter warning
+    timer_ticks++;
+
+    if ((double)timer_ticks / 18.222 > seconds_passed)
+    {
+        seconds_passed++;
+    }
+}
+
+/// @brief To be called in kernel main
+/// @param
+void timer_install(void)
+{
+    register_request_handler(0, timer_handler);
+}
+
+/// @brief Loop until given time passes; be ware that accepted only in ticks and not in seconds
+/// @param ticks
+void timer_wait(int ticks)
+{
+    unsigned long eticks;
+    eticks = timer_ticks + ticks;
+    while (timer_ticks < eticks)
+        ;
+}
---
+++ kernel/include/kernel/timer.h
@@ -0,0 +1,7 @@
+#ifndef _KERNEL_TIMER_H
+#define _KERNEL_TIMER_H
+
+void timer_install(void);
+void timer_wait(int);
+
+#endif
