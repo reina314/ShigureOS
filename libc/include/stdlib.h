924093e0588f79cd5d3761c9e699a90c03fa4ce6
reina314 <ryugureina314@gmail.com>
2025-02-21 03:44:12 +0900

    update: implement round_up_to_multiple

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -14,6 +14,19 @@ extern "C"
     void outb(uint16_t port, uint8_t value); // defined in io.c
     uint8_t inb(uint16_t port);              // defined in io.c
 
+    unsigned int round_up_to_multiple(unsigned int, unsigned int); // defined in round.c
+
+    typedef void *type_t;
+    typedef char (*lessthan_predicate_t)(type_t, type_t);
+    /// @brief Insertion sorted array; always remains in a sorted state between cells; pointer or anything
+    typedef struct ordered_array
+    {
+        type_t *array;
+        uint32_t size;
+        uint32_t max_size;
+        lessthan_predicate_t less_than;
+    } ordered_array_t;
+
 #ifdef __cplusplus
 }
 #endif
