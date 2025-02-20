6cbc1f72b5110b7d0dd5d626a40bee7a985728ad
reina314 <ryugureina314@gmail.com>
2025-02-20 19:03:16 +0900

    update: implement timer_install

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,11 +4,12 @@
 #include <kernel/idt.h>
 #include <kernel/isr.h>
 #include <kernel/irq.h>
+#include <kernel/timer.h>
 #include <kernel/serial.h>
 #include <kernel/tty.h>
 #include <kernel/kb.h>
 
-void kernel_main(void)
+int kernel_main(void)
 {
 	terminal_initialize();
 	gdt_install();
@@ -16,12 +17,14 @@ void kernel_main(void)
 	isr_install();
 	irq_install();
 	serial_initialize(SERIAL_COM1_BASE);
+	timer_install();
 	keyboard_install();
-	printf("Everything setup correctly\n");
 
 	asm volatile("sti");
 	__asm__ volatile("sti"); // Enable interrupts
 
-	while (1)
+	for (;;)
 		;
+
+	return 0;
 }
