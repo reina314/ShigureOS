556398802e294d829372604e86e1c544bcc94e96
reina314 <ryugureina314@gmail.com>
2025-03-01 19:30:28 +0900

    update: add ALIGN macro

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -18,8 +18,6 @@ extern "C"
     void outb(uint16_t port, uint8_t value); // defined in io.c
     uint8_t inb(uint16_t port);              // defined in io.c
 
-    unsigned int round_up_to_multiple(unsigned int, unsigned int); // defined in round.c
-
 // Invoke panic and cause the kernel into infinite loop
 #define PANIC(msg) panic(msg, __FILE__, __LINE__);
 // Evaluate the expression and invoke panic if false
@@ -28,25 +26,8 @@ extern "C"
     extern void panic(const char *, const char *, uint32_t);
     extern void panic_assert(const char *, uint32_t, const char *);
 
-    typedef void *type_t;
-    typedef int8_t (*lessthan_predicate_t)(type_t, type_t);
-    /// @brief Insertion sorted array; always remains in a sorted state between cells; pointer or anything
-    typedef struct ordered_array
-    {
-        type_t *array;
-        uint32_t size;
-        uint32_t max_size;
-        lessthan_predicate_t less_than;
-    } ordered_array_t;
-
-    // Create an ordered array
-    ordered_array_t create_ordered_array(uint32_t, lessthan_predicate_t);
-    ordered_array_t place_ordered_array(void *, uint32_t, lessthan_predicate_t);
-
-    // Manipulate an ordered array
-    void insert_ordered_array(type_t, ordered_array_t *);     // defined in ordered_array.c
-    void remove_ordered_array(uint32_t, ordered_array_t *);   // defined in ordered_array.c
-    type_t lookup_ordered_array(uint32_t, ordered_array_t *); // defined in ordered_array.c
+// Round up a value to the nearest alignment boundary
+#define ALIGN(value, alignment) (((value) + (alignment - 1)) & ~(alignment - 1))
 
 #ifdef __cplusplus
 }
