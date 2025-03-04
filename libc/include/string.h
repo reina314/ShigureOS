47b14ff4b231c6738d54ffdd9ea2270d7c5ffcfe
reina314 <ryugureina314@gmail.com>
2025-03-04 17:47:32 +0900

    update: add str functions

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,14 +6,17 @@
 #include <stddef.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-int memcmp(const void*, const void*, size_t);
-void* memcpy(void* __restrict, const void* __restrict, size_t);
-void* memmove(void*, const void*, size_t);
-void* memset(void*, int, size_t);
-size_t strlen(const char*);
+    int memcmp(const void *, const void *, size_t);
+    void *memcpy(void *__restrict, const void *__restrict, size_t);
+    void *memmove(void *, const void *, size_t);
+    void *memset(void *, int, size_t);
+    size_t strlen(const char *);
+    void strcpy(char *dstptr, const char *srcptr);
+    int strcmp(const char *aptr, const char *bptr);
 
 #ifdef __cplusplus
 }
